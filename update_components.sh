8c84a99a2b7fa12675023c8c95c22845bc286e44
KovalM <michail.kovalev7@gmail.com>
2016-10-13 22:21:01 +0300

    remove not used files

--- update_components.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cp -r kb/graph_drawings/ ../kb/
-rsync --recursive sc-web/ ../sc-web/
-cat files_for_addition/add_to_common.html>>../sc-web/client/templates/common.html
-cat files_for_addition/add_to_components.html>>../sc-web/client/templates/components.html
