09130c0f63c7c54f9b02b25925d7f13ee8bf67dd
Alex <san91130324@gmail.com>
2016-11-12 21:04:05 +0300

    add loop and fix edge connection

--- sc-web/components/scgg/src/listener/scgg-mode-edge.js
+++ sc-web/components/scgg/src/listener/scgg-mode-edge.js
@@ -28,6 +28,8 @@ SCggEdgeListener.prototype = {
     },
 
     onMouseDownObject: function (obj) {
+        if(obj instanceof SCgg.ModelEdge)
+            return false;
         var scene = this.scene;
         if (!scene.edge_data.source) {
             scene.edge_data.source = obj;
@@ -38,27 +40,23 @@ SCggEdgeListener.prototype = {
             });
             return true;
         } else {
-            // source and target must be not equal
-            if (scene.edge_data.source != obj) {
-                if (!(obj instanceof SCgg.ModelContour && obj.isNodeInPolygon(scene.edge_data.source))) {
-                    scene.commandManager.execute(new SCggCommandCreateEdge(scene.edge_data.source,
-                        obj,
-                        this.scene));
-                    return true;
-                } else {
-                    scene.drag_line_points.push({
-                        x: scene.mouse_pos.x,
-                        y: scene.mouse_pos.y,
-                        idx: scene.drag_line_points.length
-                    });
-                    return true;
-                }
-            } else {
-                scene.edge_data.source = scene.edge_data.target = null;
-                scene.drag_line_points.splice(0, scene.drag_line_points.length);
-                scene.clearSelection();
-                scene.appendSelection(obj);
+
+            if (scene.edge_data.source == obj && scene.drag_line_points.length==1) {
+                scene.drag_line_points.push({
+                    x: scene.mouse_pos.x+40,
+                    y: scene.mouse_pos.y-40,
+                    idx: scene.drag_line_points.length
+                });
+                scene.drag_line_points.push({
+                    x: scene.mouse_pos.x-40,
+                    y: scene.mouse_pos.y-40,
+                    idx: scene.drag_line_points.length
+                });
             }
+            scene.commandManager.execute(new SCggCommandCreateEdge(
+                scene.edge_data.source,
+                obj,
+                this.scene));
         }
         return false;
     },
